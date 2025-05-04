e57ed421e38c2a5d809cfd1bc94a109fe5b5e24a
Дима <dmitriimaltsev@mail.ru>
2025-05-04 10:20:06 +0300

    1

---
+++ config.py
@@ -0,0 +1,4 @@
+# config.py
+BOT_TOKEN = "7178786052:AAHkPKEDCgTVVtbxt0uPLTUaR1eNh623Ru4" # стилист @experthelp_bot
+#BOT_TOKEN = "8089456272:AAGfI76rAHSPJbp5SWAO-WYXzzeLwINAtJg" # бизнес @tradebotdeema_bot
+OPENAI_API_KEY = "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"
